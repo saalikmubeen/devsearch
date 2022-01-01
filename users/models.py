3841002458dc28ed6aa20ad4fc1cdb5df14193a0
saalik123 <63722568+saalik123@users.noreply.github.com>
2022-01-02 04:45:02 +0530

    added Skill model

--- users/models.py
+++ users/models.py
@@ -23,3 +23,11 @@ class Profile(models.Model):
     
     def __str__(self):
         return str(self.username)
+    
+    
+class Skill(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    description = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
