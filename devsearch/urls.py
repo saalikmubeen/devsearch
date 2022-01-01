3ff7b17652579e70cbde90e2132afbe19954aa58
saalik123 <63722568+saalik123@users.noreply.github.com>
2022-01-01 20:27:05 +0530

    added projects page

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -21,7 +21,8 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("users.urls"))
+    path("", include("users.urls")),
+    path("projects/", include("projects.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
+
+def projects(request):
+    projects = Project.objects.all()
+    return render(request, "projects/projects.html", {"projects": projects})
+
+
