7333340f83668d372e532b6089b4d9581b5f76d4
saalik123 <63722568+saalik123@users.noreply.github.com>
2022-01-02 19:06:23 +0530

    added seding welcome emails

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -137,4 +137,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'salikmubien@gmail.com'
-EMAIL_HOST_PASSWORD = '<your-password>'
+EMAIL_HOST_PASSWORD = 'your_password'
--- users/signals.py
+++ users/signals.py
@@ -1,6 +1,8 @@
 from django.db.models.signals import post_save, post_delete
 from django.contrib.auth.models import User
 from django.dispatch import receiver
+from django.core.mail import send_mail
+from django.conf import settings
 from .models import Profile
 
 
@@ -8,7 +10,7 @@ from .models import Profile
 def createProfile(sender, instance, created, **kwargs):
     if created:
         user = instance
-        Profile.objects.create(
+        profile = Profile.objects.create(
             user=user,
             name=user.first_name,
             email=user.email,
@@ -17,11 +19,22 @@ def createProfile(sender, instance, created, **kwargs):
 
         print("Profile created")
 
+        subject = "Welcome to DevSearch"
+        message = "Thank you for signing up to DevSearch. We hope you enjoy using it."
+
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False,
+        )
+
 
 # sender = Profile, instance = instance of Profile, created = True or False
 def deleteUser(sender, instance, **kwargs):
     try:
-        user = instance.user # profile.user
+        user = instance.user  # profile.user
         user.delete()
         print("User deleted")
     except:
