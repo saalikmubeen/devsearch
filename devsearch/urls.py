b013d1c932f19e4394704e0236f4c70d5b95bcc4
saalik123 <63722568+saalik123@users.noreply.github.com>
2022-01-02 18:58:00 +0530

    customized password reset templates

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -25,10 +25,16 @@ urlpatterns = [
     path("", include("users.urls")),
     path("projects/", include("projects.urls")),
     
-    path('reset_password/', PasswordResetView.as_view(), name='reset_password'),
-    path('reset_password_sent', PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset_password_complete', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('reset_password/', PasswordResetView.as_view(template_name="reset_password.html"), name='reset_password'),
+    
+    path('reset_password_sent', PasswordResetDoneView.as_view(
+        template_name="reset_password_sent.html"), name='password_reset_done'),
+    
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(
+        template_name="reset_form.html"), name='password_reset_confirm'),
+    
+    path('reset_password_complete', PasswordResetCompleteView.as_view(template_name="reset_password_complete.html"), 
+        name = 'password_reset_complete'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
