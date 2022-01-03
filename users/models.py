07491e1213aa7c2600abd0ccc0f4fb6a943de231
saalik123 <63722568+saalik123@users.noreply.github.com>
2022-01-03 18:12:31 +0530

    added Message model

--- users/models.py
+++ users/models.py
@@ -34,3 +34,23 @@ class Skill(models.Model):
 
     def __str__(self):
         return self.name
+    
+    
+class Message(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    sender = models.ForeignKey(
+        Profile, on_delete=models.SET_NULL, null=True, blank=True)
+    recipient = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name='received_messages')
+    name = models.CharField(max_length=200, null=True, blank=True)
+    email = models.EmailField(max_length=200, null=True, blank=True)
+    subject = models.CharField(max_length=200, null=True, blank=True)
+    body = models.TextField()
+    is_read = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.subject
+
+    class Meta:
+        ordering = ['is_read', '-created']
