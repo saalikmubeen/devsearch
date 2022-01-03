005ed1bbd353a9c07d0c0893e03f3c616adf91fd
saalik123 <63722568+saalik123@users.noreply.github.com>
2022-01-04 03:22:28 +0530

    fixed pagination + search projects

--- static/uikit/app.js
+++ static/uikit/app.js
@@ -1,9 +1,3 @@
-// Invoke Functions Call on Document Loaded
-// document.addEventListener('DOMContentLoaded', function () {
-//   hljs.highlightAll();
-// });
-
-
 let alertWrapper = document.querySelector('.alert')
 let alertClose = document.querySelector('.alert__close')
 
